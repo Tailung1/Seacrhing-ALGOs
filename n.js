484a68100a771a922dacdeec467994313fb92365
Tailung1 <ramazsamitashvili11@gmail.com>
2025-04-15 00:08:40 +0400

    Binary Search DONE

--- n.js
+++ n.js
@@ -12,3 +12,26 @@
 // }
 
 // console.log(findTarget(arr, 642));
+
+// Binary Search
+
+let arr = [1, 4, 7, 9, 13, 34, 233];
+
+function findTarget(arr, target) {
+  let low = 0;
+  let high = arr.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] > target) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return -1;
+}
+
+console.log(findTarget(arr, 13));
