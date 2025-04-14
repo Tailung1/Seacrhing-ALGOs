95ca84f6da7fa228e40ff154d987530dc9683a4a
Tailung1 <ramazsamitashvili11@gmail.com>
2025-04-14 23:22:32 +0400

    linear Search DONE

---
+++ n.js
@@ -0,0 +1,14 @@
+// Linear Search --- O(n)
+
+// let arr = [4, 23, 12, 64, 7, 7, 1];
+
+// function findTarget(array, target) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === target) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+
+// console.log(findTarget(arr, 642));
